ac6a26298a388c9189fc2ba05b6f1647d94b05a4
Justin Huang <yinting_justin_huang@outlook.com>
2026-03-10 10:49:32 +0000

    change to per core eviction

--- prefetcher/l1_sms_eviction/sms.cc
+++ prefetcher/l1_sms_eviction/sms.cc
@@ -40,9 +40,8 @@ uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cac
 uint32_t CACHE::prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in) {
     uint64_t evicted_block = evicted_addr >> LOG2_BLOCK_SIZE;
 
-    /* inform all sms modules of the eviction */
-    for (int i = 0; i < NUM_CPUS; i += 1)
-        prefetchers[i].eviction(evicted_block);
+    prefetchers[cpu].eviction(evicted_block);
+
     return metadata_in;
 }
 
--- prefetcher/l2_pmp_eviction/pmp.cc
+++ prefetcher/l2_pmp_eviction/pmp.cc
@@ -275,9 +275,8 @@ uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cac
 uint32_t CACHE::prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in) {
     uint64_t evicted_block_number = evicted_addr >> LOG2_BLOCK_SIZE;
 
-    for (int i = 0; i < NUM_CPUS; i += 1) {
-        prefetchers[i].eviction(evicted_block_number);
-    }
+    prefetchers[cpu].eviction(evicted_block_number);
+
     return metadata_in;
 }
 
