2c1828b9191d58ed433c65fc74ad0b4baede8450
Justin Huang <yinting_justin_huang@outlook.com>
2026-03-26 18:43:27 +0800

    add bingo hit rate tracking and history table capacity sweep

--- prefetcher/l2_bingo_eviction/bingo.cc
+++ prefetcher/l2_bingo_eviction/bingo.cc
@@ -21,7 +21,7 @@ void CACHE::prefetcher_initialize() {
     std::cout << NAME << " Bingo with PrefetchBuffer prefetcher" << std::endl;
 
     prefetchers = std::vector<bingo_pb::Bingo>(NUM_CPUS, bingo_pb::Bingo(bingo_pb::REGION_SIZE >> LOG2_BLOCK_SIZE, bingo_pb::MIN_ADDR_WIDTH, bingo_pb::MAX_ADDR_WIDTH,
-                                                                         bingo_pb::PC_WIDTH, bingo_pb::FT_SIZE, bingo_pb::FT_WAY, bingo_pb::AT_SIZE, bingo_pb::AT_WAY, bingo_pb::PHT_SIZE, bingo_pb::PHT_WAY, bingo_pb::PB_SIZE, bingo_pb::PB_WAY, 0, this));
+                                                                         bingo_pb::PC_WIDTH, bingo_pb::FT_SIZE, bingo_pb::FT_WAY, bingo_pb::AT_SIZE, bingo_pb::AT_WAY, bingo_pb::PHT_SIZE, bingo_pb::PHT_WAY, bingo_pb::PB_SIZE, bingo_pb::PB_WAY, 0));
 }
 
 uint32_t CACHE::prefetcher_cache_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type, uint32_t metadata_in) {
