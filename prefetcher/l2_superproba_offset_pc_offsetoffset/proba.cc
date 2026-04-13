3c60b988676278a2c321f3086f1461d15aa8a35b
Justin Huang <yinting_justin_huang@outlook.com>
2026-04-13 15:17:55 +0100

    add superproba debug message

--- prefetcher/l2_superproba_offset_pc_offsetoffset/proba.cc
+++ prefetcher/l2_superproba_offset_pc_offsetoffset/proba.cc
@@ -330,11 +330,11 @@ uint64_t PrefetchBuffer::build_key(uint64_t region_num) {
 Proba::Proba(int agt_size, int agt_ways, int ft_size, int ft_ways, int pht_size, int pht_ways, int width, int jt_size, int pb_size, int pb_ways, bool is_debug, int cpu) :
     agt(agt_size, agt_ways),
       phts(std::vector<PatternHistoryTable>{
+          PatternHistoryTable(pht_size, pht_ways, width, PatternHistoryTable::Behavior::OffsetOffset),
           PatternHistoryTable(pht_size, pht_ways, width, PatternHistoryTable::Behavior::PC),
         //   PatternHistoryTable(pht_size, pht_ways, width, PatternHistoryTable::Behavior::PCOffset),
         //   PatternHistoryTable(pht_size*pht_size, pht_ways, width, PatternHistoryTable::Behavior::PCAddr),
-          PatternHistoryTable(NUM_BLOCKS, 1, width, PatternHistoryTable::Behavior::Offset),
-          PatternHistoryTable(pht_size, pht_ways, width, PatternHistoryTable::Behavior::OffsetOffset)
+          PatternHistoryTable(NUM_BLOCKS, 1, width, PatternHistoryTable::Behavior::Offset)
       }),
 jt(jt_size),ft(ft_size, ft_ways), pb(pb_size, NUM_BLOCKS, 0, pb_ways), is_debug(is_debug), cpu(cpu) {
 }
