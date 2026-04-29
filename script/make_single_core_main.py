c1cc79f77aab943ea425acc23cdb7461891d1d63
Justin Huang <yinting_justin_huang@outlook.com>
2026-04-29 21:17:34 +0100

    fix multicore cloudsuite

--- script/make_single_core_main.py
+++ script/make_single_core_main.py
@@ -7,7 +7,7 @@ def main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir('../../Proba_Champsim')
     
-    for prefetcher in ['no','l2_sms','l2_sms_train_on_misstaghit','l2_proba_pc_offset','l2_proba_pcoffset_eog_jail_sampling','l2_proba_pc_pcoffset_offsetoffset_eog_jail_sampling','l2_superproba_pc_pcoffset_offsetoffset']:
+    for prefetcher in ['no','l2_sms','l2_bingo','l2_dspatch','l2_pmp','l2_gaze','l2_superproba_pc_pcoffset_offsetoffset_80_80']:
         make_1core_l2_prefetcher(prefetcher)
     
     print('Done.')
